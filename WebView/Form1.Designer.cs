aae4a385c48c2321222a94122bc35cce8ea71e36
v.ponomarenko <v.ponomarenko@traderevolution.com>
2020-03-18 16:58:16 +0200

    Add files

---
+++ WebView/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Toolkit.Forms.UI.Controls;
+
+namespace WebViewBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+#pragma warning disable CS0618 // Type or member is obsolete
+            this._webview = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+#pragma warning restore CS0618 // Type or member is obsolete
+            ((System.ComponentModel.ISupportInitialize)(this._webview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1170, 20);
+            this.textBox1.TabIndex = 0;
+            textBox1.KeyDown += TextBox1_KeyDown;
+            // 
+            // _webview
+            // 
+            this._webview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._webview.Location = new System.Drawing.Point(0, 20);
+            this._webview.Margin = new System.Windows.Forms.Padding(0, 50, 0, 0);
+            this._webview.Name = "_webview";
+            this._webview.Size = new System.Drawing.Size(1170, 556);
+            this._webview.TabIndex = 0;
+            _webview.NavigationCompleted += _webview_NavigationCompleted;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 576);
+            this.Controls.Add(this._webview);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._webview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+#pragma warning disable CS0618 // Type or member is obsolete
+        private WebView _webview;
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
+
---
+++ WebView/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Toolkit.Forms.UI.Controls;
+
+namespace WebViewBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void TextBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter || string.IsNullOrEmpty(textBox1.Text))
+                return;
+            try
+            {
+                Uri url = new Uri(textBox1.Text.StartsWith("http") ? textBox1.Text : $"http://{textBox1.Text}");
+                _webview.Source = url;
+            }
+            catch  { }         
+        }
+
+        private void _webview_NavigationCompleted(object sender, WebViewControlNavigationCompletedEventArgs e)
+        {
+            textBox1.Text = e.Uri.AbsoluteUri;
+        }
+    }
+}
